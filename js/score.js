b1e8cc91fb5ff682a1c154a6ef77e834ecb6508d
Dmitriy Karaush <dkaraush@gmail.com>
2021-07-03 23:35:10 +0300

    score animation from sin function

--- js/score.js
+++ js/score.js
@@ -1,6 +1,8 @@
 function GameScore() {
     state.scoreObject.text = state.score
-    if(state.scoreAdd) {
+    if (state.scoreAdd) {
+
+        AddScoreAnimation();
         state.score += 1;
     }
     state.scoreAdd = false;
@@ -13,21 +15,33 @@ function ScoreAnimPlus() {
         state.scoreObject.style.stroke = 'rgba(0,128,0,' + state.scoreColorAlpha + ')';
 
     }
-    if(state.scoreObject.y >= state.fontSize) {
-        state.scoreSpeed = -10;
-        state.scoreAnimSwitch = true;
+    // if(state.scoreObject.y >= state.fontSize) {
+    //     state.scoreSpeed = -10;
+    //     state.scoreAnimSwitch = true;
 
-    }
-    if(state.gravity < 2) {
-        state.scoreObject.y += state.scoreSpeed;
-    }
+    // }
+    // if(state.gravity < 2) {
+    //     state.scoreObject.y += state.scoreSpeed;
+    // }
 
-    if(state.scoreAnimSwitch && state.scoreSpeed < 15) {
-        state.scoreSpeed += state.gravity;
-        state.gravity += 0.04;
+    // if(state.scoreAnimSwitch && state.scoreSpeed < 15) {
+    //     state.scoreSpeed += state.gravity;
+    //     state.gravity += 0.04;
 
-    }
+    // }
+
+    const duration = 150
+    const time = Date.now()
+
+    if (time > state.scoreAnimationStart + duration)
+        state.scoreObject.y = state.fontSize
+    else
+        state.scoreObject.y = state.fontSize - Math.sin( (time - state.scoreAnimationStart) / duration * Math.PI ) * state.fontSize * 0.2
 }
+function AddScoreAnimation() {
+    state.scoreAnimationStart = Date.now();
+}
+
 
 function ScoreVariablesRemove() {
     if(state.scoreAnimRemoveVariables) {
