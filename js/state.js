f427222e7563b7ace92b4514e687584e528c387d
Dmitriy Karaush <dkaraush@gmail.com>
2021-10-17 15:46:19 +0300

    drawing plus using lineTo (indev)

--- js/state.js
+++ js/state.js
@@ -1,3 +1,114 @@
+// source: https://stackoverflow.com/a/52498612/4159122
+PIXI.Graphics.prototype.drawDashLine = function(toX, toY, dash = 16, gap = 8) {
+  const lastPosition = this.currentPath.points;
+
+  const currentPosition = {
+    x: lastPosition[lastPosition.length - 2] || 0,
+    y: lastPosition[lastPosition.length - 1] || 0
+  };
+
+  const absValues = {
+    toX: Math.abs(toX),
+    toY: Math.abs(toY)
+  };
+
+  for (
+    ;
+    Math.abs(currentPosition.x) < absValues.toX ||
+    Math.abs(currentPosition.y) < absValues.toY;
+  ) {
+    currentPosition.x =
+      Math.abs(currentPosition.x + dash) < absValues.toX
+        ? currentPosition.x + dash
+        : toX;
+    currentPosition.y =
+      Math.abs(currentPosition.y + dash) < absValues.toY
+        ? currentPosition.y + dash
+        : toY;
+
+    this.lineTo(currentPosition.x, currentPosition.y);
+
+    currentPosition.x =
+      Math.abs(currentPosition.x + gap) < absValues.toX
+        ? currentPosition.x + gap
+        : toX;
+    currentPosition.y =
+      Math.abs(currentPosition.y + gap) < absValues.toY
+        ? currentPosition.y + gap
+        : toY;
+
+    this.moveTo(currentPosition.x, currentPosition.y);
+  }
+};
+const MakeGreyPlus = () => {
+  const G = new PIXI.Graphics();
+  // G.beginFill(0x5d0015);
+  G.lineStyle({
+    width: 8 / window.devicePixelRatio,
+    color: 0x3D3D3D,
+    cap: PIXI.LINE_CAP.ROUND,
+    miterLimit: 200
+  });
+  const fontSize = Width * s / 10,  
+        width = 0.5927 * fontSize;
+  const dash = 2, gap = 6;
+  G.moveTo(0, 0);
+  G.drawDashLine(0, 0, dash, gap);
+  G.drawDashLine(0, fontSize, dash, gap);
+  G.drawDashLine(width, fontSize, dash, gap);
+  G.drawDashLine(70, 200, dash, gap);
+
+  let cx = 0, cy = 0;
+  const M = (x, y) => G.moveTo(cx = x, cy = y);
+  const V = (y) => G.drawDashLine(cx, cy = y);
+  const H = (x) => G.drawDashLine(cx = x, cy);
+  M(14.4883, 36.582);
+  V(23.1875);
+  H(0.988281);
+  V(13.9414);
+  H(14.4883);
+  V(0.546875);
+  H(23.4883);
+  V(13.9414);
+  H(37.0234);
+  V(23.1875);
+  H(23.4883);
+  V(36.582);
+  H(14.4883);
+
+  /**
+   * 
+   * <svg width="38" height="37" viewBox="0 0 38 37" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="
+  M14.4883 36.582
+  V23.1875
+  H0.988281
+  V13.9414
+  H14.4883
+  V0.546875
+  H23.4883
+  V13.9414
+  H37.0234
+  V23.1875
+  H23.4883
+  V36.582
+  H14.4883
+  Z
+</svg>
+
+   */
+  return G;
+  /*
+      // fill: '#FFD700',
+      lineJoin: 'round',
+      stroke: '#3D3D3D',
+      fontSize: Width * s / 10 - 8 / window.devicePixelRatio * 1.25,
+      fontWeight: 'bold',
+      strokeThickness: ,
+   */
+  return new PIXI.Text('+', config.styles.grayPlus);
+}
+
 let state = {
   startText: 0,
   shRealSize: PIXI.TextMetrics.measureText('Ш', config.styles.sh),
@@ -5,7 +116,7 @@ let state = {
 
   scoreObject: new PIXI.Text('0', config.styles.score),
   sh: new PIXI.Text('Ш', config.styles.sh),
-  greyPlus: new PIXI.Text('+', config.styles.grayPlus),
+  greyPlus: MakeGreyPlus(),
   fontSize: Width / 10,
   deadPlus: {
     value: 0,
@@ -107,7 +218,7 @@ function initialization() {
     plusRealSize: PIXI.TextMetrics.measureText('+', config.styles.greenPlus),
     scoreObject: new PIXI.Text('0', config.styles.score),
     sh: new PIXI.Text('Ш', config.styles.sh),
-    greyPlus: new PIXI.Text('+', config.styles.grayPlus),
+    greyPlus: MakeGreyPlus(),
     fontSize: Width / 10,
     deadPlus: {
       value: 0,
